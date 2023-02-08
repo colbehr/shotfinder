8a14753cc9274238034b9bf84c35c4d2c1a4f4ed
colbehr <colbehr@gmail.com>
2023-02-08 14:49:37 -0800

    inital commit with basic page routes

---
+++ src/pages/search.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function search() {
+    return (
+    <>
+        <h1>Search Page</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+    </>
+)}
+  
---
+++ src/pages/upload.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+export default function upload() {
+    return (
+    <>
+        <h1>Upload Page</h1>
+        <h2>
+            <Link href="/">Back to home</Link>
+        </h2>
+    </>
+)}
+  
